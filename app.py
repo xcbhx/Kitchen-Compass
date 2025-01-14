56c8ecafb24b35c0608ef5dd61018ac35f33e713
Ceina Ellison <ceina.ellison@gmail.com>
2025-01-14 12:16:15 -0500

    set up link pages

--- app.py
+++ app.py
@@ -6,10 +6,25 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/breakfast")
+def breakfast():
+    return render_template("breakfast.html")
 
+@app.route("/lunch")
+def lunch():
+    return render_template("lunch.html")
 
+@app.route("/dinner")
+def dinner():
+    return render_template("dinner.html")
 
+@app.route("/sidedishes")
+def side_dishes():
+    return render_template("sidedishes.html")
 
+@app.route("/dessert")
+def dessert():
+    return render_template("dessert.html")
 
 
 
